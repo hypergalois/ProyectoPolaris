ac9564348282729b49e123bda9529672bf3adc6c
hypergalois <jose.delgado2@live.u-tad.com>
2024-02-11 05:48:21 +0100

    Scaffolded App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+
+import NavBar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+import ProfilePage from "./pages/ProfilePage";
+import ProjectFormPage from "./pages/ProjectFormPage";
+
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
-  
-
   return (
-    <>
-      <div>
-        <h1>Proyecto Polaris</h1>
-      </div>
-    </>
+    <AuthProvider>
+      <Router>
+        <main>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <ProtectedRoute path="/profile" element={<ProfilePage />} />
+            <ProtectedRoute path="/project/new" element={<ProjectFormPage />} />
+          </Routes>
+        </main>
+      </Router>
+    </AuthProvider>
   )
 }
 
