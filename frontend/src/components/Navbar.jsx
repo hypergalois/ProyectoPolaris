69c959b0679f8c5c1ff929803564336135e34686
hypergalois <jose.delgado2@live.u-tad.com>
2024-02-11 05:43:17 +0100

    Created basic pages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,41 @@
 // TODO Mejorar Navbar
+import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 function NavBar() {
+
+    // Yago, Aitor, dependiendo de si el usuario esta logeado o no, haremos conditional rendering
+    // y mostraremos unos botones u otros
+    // Para acceder al estado de autenticacion, usamos el hook useAuth
+
+    const { isAuthenticated, logout } = useAuth();
+
     return (
         <nav>
+            <div>
+                <Link to="/">UTAD PROYECTOS</Link>
+            </div>
             <ul>
-                <li>Hola</li>
+                {isAuthenticated ? (
+                    <React.Fragment>
+                        <li>
+                            <Link to="/projects/add">Añadir proyecto</Link>
+                        </li>
+                        <li>
+                            <Link to="/logout">Logout</Link>
+                        </li>
+                    </React.Fragment>
+                ) :
+                    (
+                        <React.Fragment>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/register">Register</Link>
+                            </li>
+                        </React.Fragment>
+                    )}
             </ul>
         </nav>
     )
---
+++ frontend/src/pages/ProjectFormPage.jsx
@@ -0,0 +1,9 @@
+const ProjectFormPage = () => {
+    return (
+        <div>
+            <h1>Project Form Page</h1>
+        </div>
+    )
+}
+
+export default ProjectFormPage;
