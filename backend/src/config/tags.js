bd72239ef49336b6ad8461ae0978126c7d6194dd
hypermoderngalois <72045733+hypermoderngalois@users.noreply.github.com>
2024-02-13 19:24:23 +0100

    Added tags file

---
+++ backend/src/config/tags.js
@@ -0,0 +1,16 @@
+const roles = {
+    ADMIN: 'admin',
+    CREATOR: 'creator',
+    USER: 'user'
+};
+
+const status = {
+    PENDING: 'pending',
+    ACCEPTED: 'accepted',
+    REJECTED: 'rejected'
+};
+
+export default {
+    roles,
+    status
+};
