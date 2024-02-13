a0bcb8c1d7b134b0a163c6b3db497997d67f9e9c
aegui13 <95858539+aegui13@users.noreply.github.com>
2024-02-13 18:32:39 +0100

    He modificado el css del login

--- frontend/src/pages/HomePageLogin.jsx
+++ frontend/src/pages/HomePageLogin.jsx
@@ -2,18 +2,29 @@ import LoginForm from "../components/LoginForm";
 import HomePageBar from "../components/HomePageBar";
 import { Link } from "react-router-dom";
 
+//class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200"
+
 const HomePage = () => {
     return (
-        <>
+        <div class="bg-[#858585] text-white">  
+            
             <HomePageBar />
-            <h1>BIENVENIDO</h1>
-            <h2>Descubre y conecta con los proyectos que están dando forma al futuro digital en U-Tad</h2>
-            <LoginForm />
-            <footer>
-                <p>¿Olvidaste tu contraseña? <a href="/forgot-password">Recupérala</a></p>
-                <p>¿Eres nuevo en U-Tad? <Link to="/register">Registrate</Link></p>
-            </footer>
-        </>
+            <div class="flex justify-center h-screen">
+                <form class="mx-auto text-center">
+                    <div class="text-4xl">
+                        <h1>BIENVENIDO</h1>
+                    </div>
+                    <div class="text-2xl">
+                        <h2>Descubre y conecta con los proyectos que están dando forma al futuro digital en U-Tad</h2>
+                    </div>    
+                    <LoginForm />
+                    <footer class="text-xl">
+                        <p>¿Olvidaste tu contraseña? <a class="underline decoration-solid decoration-1" href="/forgot-password">Recupérala</a></p>
+                        <p>¿Eres nuevo en U-Tad? <Link class="underline decoration-solid decoration-1" to="/register">Registrate</Link></p>
+                    </footer>
+                </form>
+            </div>
+        </div>
     )
 }
 
