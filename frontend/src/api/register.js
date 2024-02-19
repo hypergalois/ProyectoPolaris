377db2df31bad14f77e7acf891882172f6a8c68c
aegui13 <95858539+aegui13@users.noreply.github.com>
2024-02-19 16:25:56 +0100

    no se necesita esto

--- frontend/src/api/register.js
+++
@@ -1,5 +0,0 @@
-import { axiosInstance } from "./axios";
-
-// TODO Crear peticiones para el registro y login de usuarios y verificacion
-
-export const degreesRequest = () => axiosInstance.get("/degrees/names/form");
