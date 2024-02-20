de304bcd1c6006e810302ac99f5a7762dd3d5e91
gonibix23 <gonibix23@gmail.com>
2024-02-20 17:56:09 +0100

    Tracing Sin Funcionar

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,7 @@
 import app from "./app.js";
-import initTracer from "./tracing.js";
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log(`App running on port ${port}`);
 });
-
-initTracer(); 
--- backend/src/tracing.js
+++ backend/src/tracing.js
@@ -1,4 +1,3 @@
-// tracing.js
 "use strict";
 import process from "process";
 import opentelemetry from "@opentelemetry/sdk-node";
@@ -7,27 +6,24 @@ import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 
-export default function initTracer() {
-    const exporterOptions = {
-        url: "http://localhost:4318/v1/traces",
-    };
+const exporterOptions = {
+    url: "http://localhost:4318/v1/traces",
+};
 
-    const traceExporter = new OTLPTraceExporter(exporterOptions);
-    const sdk = new opentelemetry.NodeSDK({
-        traceExporter,
-        instrumentations: [getNodeAutoInstrumentations()],
-        resource: new Resource({
-            [SemanticResourceAttributes.SERVICE_NAME]:
-                "proyecto-polaris-backend",
-        }),
-    });
+const traceExporter = new OTLPTraceExporter(exporterOptions);
+const sdk = new opentelemetry.NodeSDK({
+    traceExporter,
+    instrumentations: [getNodeAutoInstrumentations()],
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: "proyecto-polaris-backend",
+    }),
+});
 
-    sdk.start();
+sdk.start();
 
-    process.on("SIGTERM", () => {
-        sdk.shutdown()
-            .then(() => console.log("Tracing terminated"))
-            .catch((error) => console.log("Error terminating tracing", error))
-            .finally(() => process.exit(0));
-    });
-}
+process.on("SIGTERM", () => {
+    sdk.shutdown()
+        .then(() => console.log("Tracing terminated"))
+        .catch((error) => console.log("Error terminating tracing", error))
+        .finally(() => process.exit(0));
+});
