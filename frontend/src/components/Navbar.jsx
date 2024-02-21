9aebddc2404cc50d72063d5687d648b8c5830b11
aegui13 <95858539+aegui13@users.noreply.github.com>
2024-02-21 17:30:05 +0100

    Modificacion del navbar y añadido a las paginas projectshome, home, y perfil

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 // TODO Mejorar Navbar
 import { useAuth } from "../context/AuthContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import React from "react";
+
 
 function NavBar() {
 	// Yago, Aitor, dependiendo de si el usuario esta logeado o no, haremos conditional rendering
@@ -9,36 +10,49 @@ function NavBar() {
 	// Para acceder al estado de autenticacion, usamos el hook useAuth
 
 	const { isAuthenticated } = useAuth();
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		navigate(`/home`);
+	};
 
 	return (
-		<nav>
-			<div>
-				<Link to="/">UTAD PROYECTOS</Link>
+		<nav className="flex justify-between items-center p-8">
+			<div className="flex items-center">
+				<img
+					src="/logo-projects.png" // Reemplaza 'url_de_tu_imagen.jpg' con la URL de tu imagen
+					alt="Logo Izquierda"
+					className="h-16 w-auto cursor-pointer"
+					onClick={handleClick}
+				/>
 			</div>
-			<ul>
+			<div className="flex items-center">
 				{isAuthenticated ? (
 					<React.Fragment>
-						<li>
+						<div>
+							<Link to="/projects">UTAD PROYECTOS</Link>
+						</div>
+						<div>
 							<Link to="/projects/new">Añadir proyecto</Link>
-						</li>
-						<li>
+						</div>
+						<div>
 							<Link to="/logout">Logout</Link>
-						</li>
-						<li>
+						</div>
+						<div>
 							<Link to="/profile">Profile</Link>
-						</li>
+						</div>
 					</React.Fragment>
 				) : (
 					<React.Fragment>
-						<li>
+						<div>
 							<Link to="/login">Login</Link>
-						</li>
-						<li>
+						</div>
+						<div>
 							<Link to="/register">Register</Link>
-						</li>
+						</div>
 					</React.Fragment>
 				)}
-			</ul>
+			</div>
 		</nav>
 	);
 }
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 const ProfilePage = () => {
 	return (
 		<div>
+			<Navbar/>
 			<h1>Profile Page</h1>
 			AQUI YA ESTAS LOGEADO BRO
 		</div>
