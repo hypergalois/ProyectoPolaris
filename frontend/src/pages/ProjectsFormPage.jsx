cbd74793ea5f5db5f28c59fa1546ea01fb2630ca
aegui13 <95858539+aegui13@users.noreply.github.com>
2024-02-21 18:04:23 +0100

    Añadido el navbar a la pagina del formularo del proyecto

--- frontend/src/pages/ProjectsFormPage.jsx
+++ frontend/src/pages/ProjectsFormPage.jsx
@@ -1,8 +1,10 @@
 import ProjectForm from "../components/ProjectForm";
+import Navbar from "../components/Navbar";
 
 const ProjectFormPage = () => {
 	return (
 		<div>
+			<Navbar/>
 			<h1>Project Form Page</h1>
 			<ProjectForm />
 		</div>
