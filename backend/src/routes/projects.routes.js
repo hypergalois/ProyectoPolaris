848fd45b1d6103827cf6f2ba0abab1f1b5eb7657
gonibix23 <gonibix23@gmail.com>
2024-02-21 19:47:52 +0100

    Añadido authRequired a ruta create project

--- backend/src/routes/projects.routes.js
+++ backend/src/routes/projects.routes.js
@@ -14,7 +14,7 @@ router.get("/projects", authRequired, getProjects);
 router.get("/projects/:id", authRequired, getProject);
 
 // validateSchema(projectSchema), authRequired
-router.post("/projects", uploadFile.array("files", 5), createProject);
+router.post("/projects", authRequired, uploadFile.array("files", 5), createProject);
 
 router.put("/projects/:id", authRequired, validateSchema(updateProjectSchema), updateProject);
 
