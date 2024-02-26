5464ffef89bccd279be32daaaa2efde3105ea1ca
hypermoderngalois <72045733+hypermoderngalois@users.noreply.github.com>
2024-02-26 23:24:33 +0100

    Added routes to frontend

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,9 +1,14 @@
 import { axiosInstance } from "./axios";
 
-export const registerRequest = (email, password, username, fullName, academicRole, academicCourse, department, promotion) => axiosInstance.post("/register", email, password, username, fullName, academicRole, academicCourse, department, promotion );
+export const registerRequest = (email, password, username, fullName, academicRole, academicCourse, department, promotion) =>
+	axiosInstance.post("/register", email, password, username, fullName, academicRole, academicCourse, department, promotion);
 
 export const loginRequest = (email, password) => axiosInstance.post("/login", email, password);
 
 export const verifyTokenRequest = () => axiosInstance.get("/verify");
 
 export const logoutRequest = () => axiosInstance.post("/logout");
+
+export const forgotPasswordRequest = (email) => axiosInstance.post("/forgot-password", email);
+
+export const resetPasswordRequest = (password, token) => axiosInstance.post("/reset-password", password, token);
