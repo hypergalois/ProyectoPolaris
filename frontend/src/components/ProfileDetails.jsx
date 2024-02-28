eb28f35ca31df029e24b654c7321eaf249e3766a
aegui13 <95858539+aegui13@users.noreply.github.com>
2024-02-28 18:45:42 +0100

    Mas modificaciones del projects profile para mostrar los proyectos

--- frontend/src/components/ProfileDetails.jsx
+++ frontend/src/components/ProfileDetails.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import { useUser } from "../context/UserContext";
-import { useProjects } from "../context/ProjectsContext";
 import { useState, useEffect } from "react";
 
 const ProfileDetails = ({ project }) => {
 
 	const { profil, getProfile, errors: profileErrors } = useUser();
-	const { projects, getProjectsByUser, errors: proyectsErrors } = useProjects();
 
 	useEffect(()  => {
         getProfile()
     }, [])
-
-	useEffect(() => {
-        if (profil.id) {
-			getProjectsByUser(profil.id);
-		}
-    }, [profil.id])
-
-
 
 	return (
 		<div className="max-w-sm rounded overflow-hidden shadow-lg">
--- frontend/src/components/ProfileProjects.jsx
+++ frontend/src/components/ProfileProjects.jsx
@@ -1,15 +1,39 @@
 import React from "react";
-import { } from "../context/UserContext";
+import { useUser } from "../context/UserContext";
+import { useProjects } from "../context/ProjectsContext";
 import { useState, useEffect } from "react";
 
 const ProfileRequests = ({ project }) => {
 
-	useEffect(() => {
+	const { profil, getProfile, errors: profileErrors } = useUser();
+	const { projects, getProjectsByUser, errors: proyectsErrors } = useProjects();
+
+	useEffect(()  => {
+        getProfile()
     }, [])
+
+	useEffect(() => {
+        if (profil.id) {
+			getProjectsByUser(profil.id);
+		}
+    }, [profil.id])
+
+	useEffect(() => {
+        if (projects) {
+			console.log("Proyectos: ",projects);
+		}
+    }, [projects])
 
 	return (
 		<div className="max-w-sm rounded overflow-hidden shadow-lg">
 			<h1>Projects</h1>
+			{projects.map((objeto) => (
+				<div key={objeto.id}>
+					<div>
+						<strong>ID: {objeto.id}</strong>
+					</div>
+				</div>
+			))}
 		</div>
 	);
 };
