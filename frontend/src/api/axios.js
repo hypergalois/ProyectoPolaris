64cfb0a47b9c8425ec1823ad6cad2ad89ca979a2
hypermoderngalois <72045733+hypermoderngalois@users.noreply.github.com>
2024-03-06 15:33:26 +0100

    Update API URL in frontend configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+// console.log("API", API);
 
 export const axiosInstance = axios.create({
 	baseURL: API,
