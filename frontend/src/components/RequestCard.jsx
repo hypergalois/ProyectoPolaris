80b331018382ef203cf21fb307b513f636e322cd
Manuel Peláez <mpelaezdelarocha@gmail.com>
2024-03-06 18:05:10 +0100

    Acerptar rechazar request

--- frontend/src/components/RequestCard.jsx
+++ frontend/src/components/RequestCard.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useState, useEffect }  from "react";
+import { useRequests } from "../context/RequestsContext";
 
 function RequestCard({ request }) {
+
+    const { acceptRequestData, rejectRequestData } = useRequests();
+
+
+    const handleAccept = () => {
+        acceptRequestData(request.id);
+    };
+
+    const handleReject = () => {
+        rejectRequestData(request.id);
+    };
+
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
             <img className="w-full" src={"http://localhost:5173/full-logo-utad.webp"} alt={request.projectTitle} />
@@ -16,10 +30,10 @@ function RequestCard({ request }) {
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Subido por: {request.requester.fullName || "No especificado"}</span>
             </div>
             <div className="px-6 py-4">
-                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2">
+                <button onClick={handleAccept} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2">
                     Aceptar
                 </button>
-                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                <button onClick={handleReject} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                     Rechazar
                 </button>
             </div>
--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect }  from "react";
 import { useRequests } from "../context/RequestsContext";
 import RequestCard from "../components/RequestCard";
+import { getRequests } from "../api/requests";
 
 function DashboardPage() {
-    const { requests, getRequestsData } = useRequests();
+    const { requests, getRequestsData, requestState } = useRequests();
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         getRequestsData().then(() => setLoading(false));
         console.log(requests);
-    }, []);
+    }, [requestState]);
 
     if (requests.length === 0) {
         return <p>No hay peticiones para mostrar</p>;
@@ -17,6 +18,20 @@ function DashboardPage() {
 
     return (
         <>
+            <div className="container mx-auto px-4">
+                <div className="flex justify-center mt-8">
+                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4">
+                        Accepted
+                    </button>
+                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-4">
+                        Rejected
+                    </button>
+                    <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                        Pending
+                    </button>
+                </div>
+            </div>
+
             <h1>Estas serían las peticiones más destacadas</h1>
             {loading ? (
                 <p>Cargando peticiones...</p>
