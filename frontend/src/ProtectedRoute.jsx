04899b6bb1b62de7d9a4a3af838166e60a09838b
hypergalois <jose.delgado2@live.u-tad.com>
2024-03-07 04:42:38 +0100

    Fix email verification redirect bug

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -17,10 +17,12 @@ function ProtectedRoute() {
 		return <Navigate to="/" replace />;
 	}
 
-	if (!loading && isAuthenticated && !isEmailVerified) {
-		console.log("Redirecting to verify email");
-		return <Navigate to="/verify-email" replace />;
-	}
+	// TODO: ESTO NO FUNCIONA Y HACE QUE SE SE VEA EN BLANCO
+	// if (!loading && isAuthenticated && !isEmailVerified) {
+	// 	// console.log(loading, isAuthenticated, isEmailVerified);
+	// 	console.log("Redirecting to verify email");
+	// 	return <Navigate to="/home" replace />;
+	// }
 
 	return (
 		<>
--- frontend/src/pages/VerifyEmailPage.jsx
+++ frontend/src/pages/VerifyEmailPage.jsx
@@ -30,9 +30,9 @@ function VerifyEmailPage() {
 				<h1 className="mt-4 text-8xl font-bold tracking-tight text-[#858585] sm:text-5xl">Verifica tu email</h1>
 				<p className="mt-6 text-base leading-7 text-gray-600">Por favor, haz click en el link que hemos enviado a tu correo (checkea el spam)</p>
 				<div className="mt-10 items-center justify-center">
-					<Button className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+					<button className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
 						Reenviar email
-					</Button>
+					</button>
 				</div>
 			</div>
 		</>
