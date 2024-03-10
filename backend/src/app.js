9d7cc737ae78ff1af62b296f90fa340634d66ea6
hypergalois <jose.delgado2@live.u-tad.com>
2024-03-10 14:47:29 +0100

    Update static file path for uploads

--- backend/src/app.js
+++ backend/src/app.js
@@ -58,7 +58,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // app.use("/uploads", authRequired, express.static("uploads"));
-app.use("/uploads", express.static("uploads"));
+app.use("/app/uploads", express.static("uploads"));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs, { explorer: true }));
 
