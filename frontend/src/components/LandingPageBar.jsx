7bb111ad05bcd87398fcd0693ab0c6903bd4ab0f
hypergalois <jose.delgado2@live.u-tad.com>
2024-03-12 00:06:02 +0100

    Update package-lock.json and logo files

--- frontend/src/components/LandingPageBar.jsx
+++ frontend/src/components/LandingPageBar.jsx
@@ -2,10 +2,11 @@ const LandingPageBar = () => {
 	return (
 		<nav className="flex justify-between items-center p-8">
 			<div className="flex items-center">
-				<img src="/logo-projects.png" alt="Logo Izquierda" className="h-16 w-auto" />
+				{/* <img src="/logo-projects.png" alt="Logo Izquierda" className="h-16 w-auto" /> */}
+				<h1 className="text-3xl font-[950] text-blue-600">U-PROJECTS</h1>
 			</div>
 			<div className="flex items-center">
-				<img src="/full-logo-utad.png" alt="Logo Derecha" className="h-16 w-auto" />
+				<img src="/full-logo-utad.webp" alt="Logo Derecha" className="h-16 w-auto" />
 			</div>
 		</nav>
 	);
