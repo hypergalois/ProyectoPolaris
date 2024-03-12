db6da1cbeac73f84f86e93add4eb30d1d3d84c91
hypergalois <jose.delgado2@live.u-tad.com>
2024-03-12 07:40:26 +0100

    Update NavBarAuth component with new navigation options and icons

--- frontend/src/components/NavBarAuth.jsx
+++ frontend/src/components/NavBarAuth.jsx
@@ -1,5 +1,5 @@
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon, ArrowRightEndOnRectangleIcon, PlusIcon, MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon, ArrowRightEndOnRectangleIcon, PlusIcon, MagnifyingGlassIcon, UserCircleIcon, DocumentPlusIcon, BellIcon } from "@heroicons/react/24/outline";
 import { Fragment } from "react";
 
 import { useAuth } from "../context/AuthContext";
@@ -21,92 +21,93 @@ function NavBar() {
 
 	if (userRole === "ADMIN") {
 		navigation = [
-			{ name: "Proyectos", href: "/home", current: false },
-			{ name: "Dashboard", href: "/dashboard", current: false },
+			{ name: "TODOS", href: "/home", current: false },
+			{ name: "ANIMACIÓN", href: "/dashboard", current: false },
+			{ name: "DISEÑO DIGITAL", href: "/dashboard", current: false },
+			{ name: "INGENIERÍA Y CIENCIAS", href: "/dashboard", current: false },
+			{ name: "VIDEOJUEGOS", href: "/dashboard", current: false },
 		];
 	}
 
 	return (
-		<Disclosure as="nav" className="bg-[#2d2d2d]">
+		<Disclosure as="nav" className="bg-blue-600 fixed z-50 w-full top-0">
 			{({ open }) => (
 				<>
-					<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-						<div className="relative flex h-16 items-center justify-between">
-							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+					<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+						<div className="relative flex items-center justify-between h-16">
+							<div className="inset-y-0 left-0 flex items-center sm:hidden mr-2">
 								<Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 hover:bg-gray-700 hover:text-white">
 									{open ? <XMarkIcon className="block h-6 w-6" /> : <Bars3Icon className="block h-6 w-6" />}
 								</Disclosure.Button>
 							</div>
-							<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+							<div className="flex-1 flex items-center justify-between">
 								<div className="flex-shrink-0 flex items-center">
-									<img className="h-8 w-auto" src="/small-logo.png" alt="Your Company" />
+									<h1 className="text-2xl font-[950] text-white">U-PROJECTS</h1>
 								</div>
-								<div className="hidden sm:ml-6 sm:block">
-									<div className="flex space-x-4">
+								<div className="hidden sm:block sm:ml-6">
+									<div className="flex space-x-3 justify-center flex-1">
 										{navigation.map((item) => (
-											<Link to={item.href} key={item.name} className={`${item.current ? "bg-gray-500 text-white" : "bg-gray-700 text-gray-100 hover:bg-gray-500 hover:text-white"} px-3 py-2 rounded-md text-sm font-medium`}>
+											<Link to={item.href} key={item.name} className={`${item.current ? "bg-white text-white" : "text-white font-semibold hover:bg-white hover:text-blue-600"} px-3 py-2 rounded-md text-sm font-medium`}>
 												{item.name}
 											</Link>
 										))}
 									</div>
 								</div>
-							</div>
-							<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-								<Link to="/projects" className="inline-flex items-center bg-blue-600 px-4 py-2 mr-2 rounded-full text-white hover:bg-blue-400 focus:outline-none">
-									Buscar Proyectos
-									<MagnifyingGlassIcon className="ml-2 h-6 w-5" aria-hidden="true" />
-								</Link>
+								<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+									<Link to="/projects/new" className="inline-flex items-center bg-white px-4 py-2 mr-2 rounded-full text-white hover:bg-blue-400 focus:outline-none">
+										<DocumentPlusIcon className="h-5 w-5 text-blue-600" aria-hidden="true" />
+									</Link>
 
-								<Link to="/projects/new" className="inline-flex items-center bg-blue-600 px-4 py-2 mr-2 rounded-full text-white hover:bg-blue-400 focus:outline-none">
-									Nuevo Proyecto
-									<PlusIcon className="ml-2 h-5 w-5" aria-hidden="true" />
-								</Link>
+									<Link to="/projects/new" className="inline-flex items-center bg-white px-4 py-2 mr-2 rounded-full text-white hover:bg-blue-400 focus:outline-none">
+										<BellIcon className="h-5 w-5 text-blue-600" aria-hidden="true" />
+									</Link>
 
-								<Menu as="div" className="ml-3 relative">
-									<Menu.Button className="bg-gray-700 p-1 rounded-full text-gray-100 hover:text-white focus:outline-none">
-										<UserCircleIcon className="h-6 w-6" aria-hidden="true" />
-									</Menu.Button>
-									<Transition
-										as={Fragment}
-										enter="transition ease-out duration-100"
-										enterFrom="transform opacity-0 scale-95"
-										enterTo="transform opacity-100 scale-100"
-										leave="transition ease-in duration-75"
-										leaveFrom="transform opacity-100 scale-100"
-										leaveTo="transform opacity-0 scale-95"
-									>
-										<Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-											<Menu.Item>
-												{({ active }) => (
-													<Link to="/profile" className={`${active ? "bg-gray-100" : ""} block px-4 py-2 text-sm text-gray-700`}>
-														Profile
-													</Link>
-												)}
-											</Menu.Item>
-											<Menu.Item>
-												{({ active }) => (
-													<Link to="/settings" className={`${active ? "bg-gray-100" : ""} block px-4 py-2 text-sm text-gray-700`}>
-														Settings
-													</Link>
-												)}
-											</Menu.Item>
-											<Menu.Item>
-												{({ active }) => (
-													<Link
-														to="/"
-														className={`${active ? "bg-gray-100" : ""} block px-4 py-2 text-sm text-gray-700`}
-														onClick={() => {
-															console.log("Logout.");
-															logout();
-														}}
-													>
-														Sign out
-													</Link>
-												)}
-											</Menu.Item>
-										</Menu.Items>
-									</Transition>
-								</Menu>
+									<Menu as="div" className="ml-3 relative">
+										<Menu.Button className="bg-white px-4 py-2 mr-2 rounded-full text-gray-100 hover:text-white focus:outline-none">
+											<UserCircleIcon className="h-5 w-5 text-blue-600" aria-hidden="true" />
+										</Menu.Button>
+										<Transition
+											as={Fragment}
+											enter="transition ease-out duration-100"
+											enterFrom="transform opacity-0 scale-95"
+											enterTo="transform opacity-100 scale-100"
+											leave="transition ease-in duration-75"
+											leaveFrom="transform opacity-100 scale-100"
+											leaveTo="transform opacity-0 scale-95"
+										>
+											<Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+												<Menu.Item>
+													{({ active }) => (
+														<Link to="/profile" className={`${active ? "bg-gray-100" : ""} block px-4 py-2 text-sm text-gray-700`}>
+															Profile
+														</Link>
+													)}
+												</Menu.Item>
+												<Menu.Item>
+													{({ active }) => (
+														<Link to="/settings" className={`${active ? "bg-gray-100" : ""} block px-4 py-2 text-sm text-gray-700`}>
+															Settings
+														</Link>
+													)}
+												</Menu.Item>
+												<Menu.Item>
+													{({ active }) => (
+														<Link
+															to="/"
+															className={`${active ? "bg-gray-100" : ""} block px-4 py-2 text-sm text-gray-700`}
+															onClick={() => {
+																console.log("Logout.");
+																logout();
+															}}
+														>
+															Sign out
+														</Link>
+													)}
+												</Menu.Item>
+											</Menu.Items>
+										</Transition>
+									</Menu>
+								</div>
 							</div>
 						</div>
 					</div>
