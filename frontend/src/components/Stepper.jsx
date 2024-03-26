d785ab0504e14336d59ab4aa8b1910719c3f29c5
hypermoderngalois <72045733+hypermoderngalois@users.noreply.github.com>
2024-03-26 05:33:27 +0100

    Applying my crazy css skillz

--- frontend/src/components/Stepper.jsx
+++ frontend/src/components/Stepper.jsx
@@ -1,21 +1,15 @@
-const Stepper = ({ steps, currentStepNumber }) => {
-	const generateStepState = (stepNumber) =>
-		steps.map((step, index) => ({
-			description: step,
-			completed: index < stepNumber,
-			highlighted: index === stepNumber,
-			selected: index <= stepNumber,
-		}));
+import React from "react";
+import "../stepper.css";
 
-	const stepperSteps = generateStepState(currentStepNumber - 1);
+const Stepper = ({ currentStep }) => {
+	const steps = ["Detalles", "Usuarios", "Elementos", "Resumen"];
 
 	return (
-		<div className="stepper-container mx-4 p-4 flex justify-between items-center">
-			{stepperSteps.map((step, index) => (
-				<div key={index} className="step-container text-center">
-					{index !== 0 && <div className={`step-line ${step.selected ? "active" : ""}`} style={{ left: index === 0 ? "50%" : "0", right: index === steps.length - 1 ? "50%" : "0" }}></div>}
-					<div className={`rounded-full step-number ${step.selected ? "selected" : ""} ${step.completed ? "completed" : ""}`}>{step.completed ? <span>✓</span> : index + 1}</div>
-					<div className="step-description mt-2 text-xs font-medium uppercase">{step.description}</div>
+		<div className="flex justify-between">
+			{steps.map((step, index) => (
+				<div key={index} className="step-item">
+					<div className="step">{index + 1}</div>
+					<p className="text-blue-500">{step}</p>
 				</div>
 			))}
 		</div>
