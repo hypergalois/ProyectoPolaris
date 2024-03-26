fa911dc203a36a0207173dbc5e47ab32761d6179
hypermoderngalois <72045733+hypermoderngalois@users.noreply.github.com>
2024-03-26 01:30:02 +0100

    Update LandingPageLogin styles and text

--- frontend/src/pages/LandingPageLogin.jsx
+++ frontend/src/pages/LandingPageLogin.jsx
@@ -13,16 +13,17 @@ const HomePage = () => {
 			<LandingPageAnimation />
 			<LandingPageBar />
 
-			<div className="flex flex-col justify-center items-center w-full px-4 ">
+			<div className="flex flex-col justify-center items-center w-full px-4">
 				<div className="mx-auto text-center">
-					<div className="text-5xl md:text-8xl font-bold mb-4 md:mb-8">
+					<div className="text-5xl text-blue-600 md:text-8xl font-bold mb-4 md:mb-8">
 						<h1>¡Bienvenido!</h1>
 					</div>
-					<div className="text-lg md:text-xl font-semibold mb-8 md:mb-16">
+
+					<div className="w-full md:w-9/12 mx-auto text-4xl md:text-3xl text-blue-600 font-semibold mb-8 md:mb-8">
 						<h2>Descubre y conecta con los proyectos que están dando forma al futuro digital en U-Tad</h2>
 					</div>
 					<LoginForm />
-					<footer className="text-sm mt-8 md:mt-12">
+					<footer className="text-sm mt-6 md:mt-6">
 						<p>
 							¿Olvidaste tu contraseña?{" "}
 							<Link className="underline decoration-solid decoration-1" to="/forgot-password">
