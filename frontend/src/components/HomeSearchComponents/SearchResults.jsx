54dada098ef0d43f60e6bd464ab7c42e253d7338
hypergalois <jose.delgado2@live.u-tad.com>
2024-04-05 21:58:05 +0200

    Add search results page layout and images

--- frontend/src/components/HomeSearchComponents/SearchResults.jsx
+++ frontend/src/components/HomeSearchComponents/SearchResults.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import ProjectCard from "../Cards/ProjectCard";
 
-function SearchResults(props) {
-	const { filteredProjects } = props;
-
+function SearchResults({ filteredProjects }) {
 	return (
 		<div className="container mx-auto">
-			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">{filteredProjects.length > 0 ? filteredProjects.map((project) => <ProjectCard key={project.id} project={project} />) : <p>No projects found.</p>}</div>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+				{filteredProjects.length > 0 ? (
+					filteredProjects.map((project) => <ProjectCard key={project.id} project={project} />)
+				) : (
+					<div className="col-span-1 sm:col-span-2 md:col-span-3 flex flex-col items-center justify-center text-center p-10">
+						<span className="text-6xl mb-4">😔</span>
+						<p className="font-bold text-blue-500 mb-6">No projects found.</p>
+						<div className="flex flex-row justify-center items-center space-x-4">
+							<img src="/fernando-alonso-3.gif" alt="Lons" className="w-1/2 max-w-md" />
+							<img src="/sad.gif" alt="Second" className="w-1/2 max-w-md" />
+						</div>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
