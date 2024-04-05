1dbc059ca77c3fcfeaf2f01426bbaa6d9abd7fe2
hypergalois <jose.delgado2@live.u-tad.com>
2024-04-05 20:44:58 +0200

    Update landing page navbar with animated logo

--- frontend/src/components/Navbars/LandingPageBar.jsx
+++ frontend/src/components/Navbars/LandingPageBar.jsx
@@ -1,8 +1,21 @@
+import { motion } from "framer-motion";
+
 const LandingPageBar = () => {
 	return (
 		<nav className="flex flex-col md:flex-row justify-between items-center px-8">
 			<div className="flex flex-col md:flex-row items-center mb-4 md:mb-0">
-				<h1 className="text-3xl font-[950] text-blue-600">U-PROJECTS</h1>
+				<motion.h1
+					className="text-3xl font-[950] text-blue-600 cursor-pointer"
+					whileHover={{
+						scale: 1.1,
+						rotate: 5,
+						textShadow: "0px 0px 8px rgb(0, 187, 255)",
+						transition: { duration: 0.3 },
+					}}
+					whileTap={{ scale: 0.9 }}
+				>
+					U-PROJECTS
+				</motion.h1>
 			</div>
 			<div className="flex items-center">
 				<img src="/full-logo-utad-by.png" alt="Logo Derecha" className="h-16 w-auto" />
