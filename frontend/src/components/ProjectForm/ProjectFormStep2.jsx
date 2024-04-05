c7136b2ff6a59bdfef7469fb90583c55950dc239
hypergalois <jose.delgado2@live.u-tad.com>
2024-04-06 01:34:39 +0200

    Refactor form inputs in ProjectFormStep2 component

--- frontend/src/components/ProjectForm/ProjectFormStep2.jsx
+++ frontend/src/components/ProjectForm/ProjectFormStep2.jsx
@@ -71,79 +71,83 @@ const ProjectFormStep2 = ({ returnStep, advanceStep, currentStep }) => {
 		<>
 			<Stepper currentStep={currentStep} />
 			<div>
-				<form onSubmit={handleSubmit(onSubmit)} className="w-full bg-white  rounded px-8 pt-6 mb-2 grid gap-4 md:grid-cols-2">
-					<div className="mb-4 md:col-span-2">
-						<label className="block text-gray-700 text-sm font-bold mb-2">Estudiantes implicados</label>
-						{studentFields.map((field, index) => (
-							<div key={field.id}>
-								<div className="flex items-center gap-2">
-									<input
-										type="email"
-										{...register(`impliedStudents.${index}.student`, {
-											required: index === 0 ? "Se requiere por lo menos un estudiante implicado" : false,
-											pattern: {
-												value: /^[\w-]+(\.[\w-]+)*@live\.u-tad\.com$/,
-												message: "Introduce una dirección de correo electrónico válida",
-											},
-										})}
-										placeholder="Estudiante implicado"
-										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-									/>
-									{index > 0 && (
-										<button
-											type="button"
-											onClick={() => {
-												removeStudent(index);
-											}}
-											className="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
-										>
-											Eliminar
-										</button>
-									)}
+				<form onSubmit={handleSubmit(onSubmit)} className="w-full bg-white  rounded px-8 pt-6 mb-2 grid gap-4 md:grid-cols-2 ">
+					<div className="mb-4 md:col-span-2 outline outline-blue-400 ">
+						<div className="m-3">
+							<label className="block text-blue-400 text-sm font-bold mb-2">Estudiantes implicados</label>
+							{studentFields.map((field, index) => (
+								<div key={field.id}>
+									<div className="flex items-center gap-2">
+										<input
+											type="email"
+											{...register(`impliedStudents.${index}.student`, {
+												required: index === 0 ? "Se requiere por lo menos un estudiante implicado" : false,
+												pattern: {
+													value: /^[\w-]+(\.[\w-]+)*@live\.u-tad\.com$/,
+													message: "Introduce una dirección de correo electrónico válida",
+												},
+											})}
+											placeholder="Estudiante implicado"
+											className="shadow appearance-none  w-full py-2 px-3 mb-2 text-blue-500 placeholder-blue-500 leading-tight focus:outline-none focus:shadow-outline outline outline-blue-400 text-xs font-bold"
+										/>
+										{index > 0 && (
+											<button
+												type="button"
+												onClick={() => {
+													removeStudent(index);
+												}}
+												className="ml-2 bg-red-700 hover:bg-red-600 text-white text-sm font-bold py-1 px-2 rounded"
+											>
+												Eliminar
+											</button>
+										)}
+									</div>
+									{errors.impliedStudents?.[index]?.student && <p className="mb-2 mt-4 text-red-	 font-semibold">{errors.impliedStudents[index].student.message}</p>}
 								</div>
-								{errors.impliedStudents?.[index]?.student && <p className="mb-2 mt-4 text-red-500 font-semibold">{errors.impliedStudents[index].student.message}</p>}
+							))}
+							<div className="flex justify-center mt-4">
+								<button type="button" onClick={() => appendStudent({ student: "" })} className="mt-2 h-8 px-3 bg-blue-600 hover:bg-blue-400 text-white font-bold text-sm">
+									Añadir estudiante
+								</button>
 							</div>
-						))}
-						<div className="flex justify-center mt-4">
-							<button type="button" onClick={() => appendStudent({ student: "" })} className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-								Añadir estudiante
-							</button>
 						</div>
 					</div>
 
-					<div className="mb-4 md:col-span-2">
-						<label className="block text-gray-700 text-sm font-bold mb-2">Profesores implicados</label>
-						{professorFields.map((field, index) => (
-							<div key={field.id}>
-								<div className="flex items-center gap-2">
-									<input
-										type="email"
-										{...register(`impliedProfessors.${index}.professor`, {
-											pattern: {
-												value: /^[\w-]+(\.[\w-]+)*@u-tad\.com$/,
-												message: "Introduce una dirección de correo electrónico válida",
-											},
-										})}
-										placeholder="Profesor implicado"
-										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-									/>
-									<button
-										type="button"
-										onClick={() => {
-											removeProfessor(index);
-										}}
-										className="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
-									>
-										Eliminar
-									</button>
+					<div className="mb-4 md:col-span-2 outline outline-blue-400">
+						<div className="m-3">
+							<label className="block text-blue-400 text-sm font-bold mb-2">Profesores implicados</label>
+							{professorFields.map((field, index) => (
+								<div key={field.id}>
+									<div className="flex items-center gap-2">
+										<input
+											type="email"
+											{...register(`impliedProfessors.${index}.professor`, {
+												pattern: {
+													value: /^[\w-]+(\.[\w-]+)*@u-tad\.com$/,
+													message: "Introduce una dirección de correo electrónico válida",
+												},
+											})}
+											placeholder="Profesor implicado"
+											className="shadow appearance-none  w-full py-2 px-3 mb-2 text-blue-500 placeholder-blue-500 leading-tight focus:outline-none focus:shadow-outline outline outline-blue-400 text-xs font-bold"
+										/>
+										<button
+											type="button"
+											onClick={() => {
+												removeProfessor(index);
+											}}
+											className="ml-2  bg-red-700 hover:bg-red-600 text-white text-sm font-bold py-1 px-2 rounded"
+										>
+											Eliminar
+										</button>
+									</div>
+									{errors.impliedProfessors?.[index]?.professor && <p className="mb-2 mt-4 text-red-500 font-semibold">{errors.impliedProfessors[index].professor.message}</p>}
 								</div>
-								{errors.impliedProfessors?.[index]?.professor && <p className="mb-2 mt-4 text-red-500 font-semibold">{errors.impliedProfessors[index].professor.message}</p>}
+							))}
+							<div className="flex justify-center mt-4">
+								<button type="button" onClick={() => appendProfessor({ professor: "" })} className="mt-2 h-8 px-3 bg-blue-600 hover:bg-blue-400 text-white font-bold text-sm">
+									Añadir profesor
+								</button>
 							</div>
-						))}
-						<div className="flex justify-center mt-4">
-							<button type="button" onClick={() => appendProfessor({ professor: "" })} className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-								Añadir profesor
-							</button>
 						</div>
 					</div>
 				</form>
