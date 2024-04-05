0fc6438eafbec11e062583f7bb8ab5cff3a786ba
hypergalois <jose.delgado2@live.u-tad.com>
2024-04-05 19:41:25 +0200

    Update navigation menu and barebones for notifications

--- frontend/src/components/NavBarAuth.jsx
+++ frontend/src/components/NavBarAuth.jsx
@@ -40,11 +40,6 @@ function NavBar() {
 		{ name: "VIDEOJUEGOS", href: "/videogames", current: false },
 	];
 
-	// TODO: CAMBIAR LAS RUTAS SEGUN SE HAGAN
-	if (userRole === "ADMIN") {
-		navigation.push({ name: "DASHBOARD", href: "/dashboard", current: false, admin: true });
-	}
-
 	navigation.forEach((item) => {
 		item.current = location.pathname === item.href;
 	});
@@ -57,9 +52,7 @@ function NavBar() {
 						<div className="mx-auto px-2 sm:px-6 lg:px-8">
 							<div className="relative flex items-center justify-between h-16">
 								<div className="inset-y-0 left-0 flex items-center sm:hidden mr-2">
-									<Disclosure.Button className="inline-flex items-center justify-center p-2 text-white hover:bg-blue-400">
-										{open ? <XMarkIcon className="block h-6 w-6" /> : <Bars3Icon className="block h-6 w-6" />}
-									</Disclosure.Button>
+									<Disclosure.Button className="inline-flex items-center justify-center p-2 text-white hover:bg-blue-400">{open ? <XMarkIcon className="block h-6 w-6" /> : <Bars3Icon className="block h-6 w-6" />}</Disclosure.Button>
 								</div>
 
 								<div className="flex-1 flex items-center justify-between">
@@ -88,20 +81,63 @@ function NavBar() {
 										</div>
 									</div>
 									<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-										<button
-											type="button"
-											onClick={handleOpenProjectFormPopup}
-											className="inline-flex items-center bg-white px-3 py-2 sm:px-4 sm:py-2 mr-2 rounded-full text-white hover:bg-blue-400 focus:outline-none"
-										>
+										{/* Aqui va el disclosure */}
+
+										{userRole === "ADMIN" && (
+											<Link to={"/dashboard"} key={"DASHBOARD"} className={"bg-red-800 text-white font-semibold hover:bg-white hover:text-blue-600 px-3 py-2 rounded-md text-sm mx-6"}>
+												DASHBOARD
+											</Link>
+										)}
+
+										<button type="button" onClick={handleOpenProjectFormPopup} className="inline-flex items-center bg-white px-3 py-2 sm:px-4 sm:py-2 mr-2 rounded-full text-white hover:bg-blue-400 focus:outline-none">
 											<DocumentPlusIcon className="h-4 w-4 sm:h-5 sm:w-5 text-blue-600" aria-hidden="true" />
 										</button>
 
-										<Link
-											to="/projects/new"
-											className="inline-flex items-center bg-white px-3 py-2 sm:px-4 sm:py-2 mr-2 rounded-full text-white hover:bg-blue-400 focus:outline-none"
-										>
-											<BellIcon className="h-4 w-4 sm:h-5 sm:w-5 text-blue-600" aria-hidden="true" />
-										</Link>
+										<Menu as="div" className="relative">
+											<Menu.Button className="inline-flex items-center bg-white px-3 py-2 sm:px-4 sm:py-2 mr-2 rounded-full text-white hover:bg-blue-400 focus:outline-none">
+												<BellIcon className="h-4 w-4 sm:h-5 sm:w-5 text-blue-600" aria-hidden="true" />
+											</Menu.Button>
+											<Transition
+												as={Fragment}
+												enter="transition ease-out duration-100"
+												enterFrom="transform opacity-0 scale-95"
+												enterTo="transform opacity-100 scale-100"
+												leave="transition ease-in duration-75"
+												leaveFrom="transform opacity-100 scale-100"
+												leaveTo="transform opacity-0 scale-95"
+											>
+												<Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+													<Menu.Item>
+														{({ active }) => (
+															<Link to="/profile" className={`${active ? "bg-gray-100" : ""} block px-4 py-2 text-sm text-gray-700`}>
+																Profile
+															</Link>
+														)}
+													</Menu.Item>
+													<Menu.Item>
+														{({ active }) => (
+															<Link to="/settings" className={`${active ? "bg-gray-100" : ""} block px-4 py-2 text-sm text-gray-700`}>
+																Settings
+															</Link>
+														)}
+													</Menu.Item>
+													<Menu.Item>
+														{({ active }) => (
+															<Link
+																to="/"
+																className={`${active ? "bg-gray-100" : ""} block px-4 py-2 text-sm text-gray-700`}
+																onClick={() => {
+																	console.log("Logout.");
+																	logout();
+																}}
+															>
+																Sign out
+															</Link>
+														)}
+													</Menu.Item>
+												</Menu.Items>
+											</Transition>
+										</Menu>
 
 										<Menu as="div" className="relative">
 											<Menu.Button className="inline-flex items-center bg-white px-3 py-2 sm:px-4 sm:py-2 mr-2 rounded-full text-white hover:bg-blue-400 focus:outline-none">
@@ -154,12 +190,7 @@ function NavBar() {
 							<Disclosure.Panel className="sm:hidden">
 								<div className="px-2 pt-2 pb-3 space-y-1 bg-blue-600 shadow-lg">
 									{navigation.map((item) => (
-										<Disclosure.Button
-											key={item.name}
-											as={Link}
-											to={item.href}
-											className="text-white hover:bg-blue-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-										>
+										<Disclosure.Button key={item.name} as={Link} to={item.href} className="text-white hover:bg-blue-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
 											{item.name}
 										</Disclosure.Button>
 									))}
