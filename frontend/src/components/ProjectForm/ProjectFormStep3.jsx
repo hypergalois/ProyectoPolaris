486681119ce7c5d17e068c81949c1330bddde15d
hypergalois <jose.delgado2@live.u-tad.com>
2024-04-07 05:05:43 +0200

    Update project form components, styles, and props

--- frontend/src/components/ProjectForm/ProjectFormStep3.jsx
+++ frontend/src/components/ProjectForm/ProjectFormStep3.jsx
@@ -3,149 +3,16 @@ import { useEffect, useState } from "react";
 import Select from "react-select";
 import { Controller, useForm, useController, useFieldArray, FormProvider } from "react-hook-form";
 
-import Stepper from "../Helpers/Stepper.jsx";
-// import { customStyles } from "../HomeSearchComponents/SearchForms.jsx";
-import { selectStylesCustom } from "../../config/util.js";
+import { useProjects } from "../../context/ProjectsContext.jsx";
 import { useAreas } from "../../context/AreasContext.jsx";
 
-// LO QUE TENIA JOSE
-// <div>Titulacion, Asignatura, Proyecto Personal, Curso Academico, Premios, Miniatura, Archivos del Proyecto, Enlaces, Palabras Clave, Memoria del Proyecto</div>
-// TODO Que cuando se seleccione un grado, se carguen las asignaturas de ese grado y viceversa
+import { courseOptions } from "../../config/util.js";
+
+import Stepper from "../Helpers/Stepper.jsx";
+
+import { selectStylesCustom } from "../../config/util.js";
 
 const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectData, projectData }) => {
-	const { degrees, getDegrees, awards, getAwards, subjects, getSubjects } = useAreas();
-	const {
-		handleSubmit,
-		control,
-		formState: { errors },
-	} = useForm();
-
-	const {
-		fields: linkFields,
-		append: appendLink,
-		remove: removeLink,
-		update: updateLink,
-	} = useFieldArray({
-		control,
-		name: "externalLinks",
-	});
-
-	// Lógica del useForm y valores por defecto
-	const methods = useForm({
-		defaultValues: async () => {
-			if (projectId) {
-				try {
-					const defaultProject = await getProject(projectId);
-					console.log(defaultProject);
-					//const defaultThumbnail = defaultProject.thumbnail ? getFile(defaultProject.thumbnail) : null;
-					const defProjVals = {
-						title: defaultProject.title ? defaultProject.title : "",
-						description: defaultProject.description ? defaultProject.description : "",
-						keywords: defaultProject.keywords ? defaultProject.keywords : [],
-						awards: defaultProject.awards ? defaultProject.awards : [],
-						personalProject: defaultProject.personalProject ? defaultProject.personalProject : false,
-						academicCourse: defaultProject.academicCourse ? defaultProject.academicCourse : "",
-						course: defaultProject.course ? defaultProject.course : "",
-						letter: defaultProject.letter ? defaultProject.letter : "",
-						//thumbnail: defaultThumbnail ? [defaultThumbnail] : [],
-						externalLinks: defaultProject.externalLinks ? defaultProject.externalLinks.map((item) => ({ link: item })) : [],
-						//uploadedContent: defaultProject.uploadedContent ? defaultProject.uploadedContent.map(filePath => getFile(filePath)) : [],
-						subject: defaultProject.subject ? defaultProject.subject : "",
-						degree: defaultProject.degree ? defaultProject.degree : "",
-						impliedStudents: defaultProject.impliedStudents ? defaultProject.impliedStudents.map((item) => ({ student: item })) : [{ student: "" }],
-						impliedProfessors: defaultProject.impliedProfessors ? defaultProject.impliedProfessors.map((item) => ({ professor: item })) : [],
-					};
-					console.log(defProjVals);
-					return defProjVals;
-				} catch {
-					console.log("Error fetching project");
-				}
-			}
-			return defaultProjectFormValues;
-		},
-	});
-
-	const { register, watch, setValue, getValues } = methods;
-
-	const createDegreeOptions = (degrees) => {
-		if (!degrees) return [];
-
-		const allDegreesOption = { label: "Todos los Grados", value: "All" };
-		const degreeOptions = degrees.map((degree) => ({
-			label: degree.name,
-			value: degree.id,
-		}));
-
-		return [allDegreesOption, ...degreeOptions];
-	};
-
-	const createSubjectOptions = (subjects) => {
-		if (!subjects) return [];
-
-		const allSubjectsOption = { label: "Todas las Asignaturas", value: "All" };
-		const subjectOptions = subjects.map((subject) => ({
-			label: subject.name,
-			value: subject.id,
-		}));
-
-		return [allSubjectsOption, ...subjectOptions];
-	};
-
-	const createAwardOptions = (awards) => {
-		if (!awards) return [];
-
-		const allAwardsOption = { label: "Todos los Premios", value: "All" };
-		const awardOptions = awards.map((award) => ({
-			label: award.name,
-			value: award.id,
-		}));
-
-		return [allAwardsOption, ...awardOptions];
-	};
-
-	const {
-		field: { value: academicCourseValue, onChange: academicCourseOnChange, ...restAcademicCourseField },
-	} = useController({
-		name: "academicCourse",
-		control,
-		rules: {
-			required: "Se requiere un curso académico",
-		},
-	});
-
-	const [academicCourseOptions, setAcademicCourseOptions] = useState([]);
-	useEffect(() => {
-		setAcademicCourseOptions(generateAcademicCourseOptions(2010));
-	}, []);
-	const generateAcademicCourseOptions = (startYear) => {
-		const currentMonth = new Date().getMonth(); // Obtener el mes actual (0-11, donde 0 es enero)
-		const currentYear = new Date().getFullYear();
-		const academicCourseOptions = [];
-
-		// Si estamos antes de septiembre, retrocedemos un año
-		const currentStartAcademicCourse = currentMonth < 8 ? currentYear - 1 : currentYear;
-
-		// Genera opciones para los cursos académicos
-		for (let year = startYear; year <= currentStartAcademicCourse; year++) {
-			const nextYear = year + 1;
-			const academicCourse = `${year}/${nextYear}`;
-			academicCourseOptions.unshift({ value: academicCourse, label: academicCourse });
-		}
-
-		return academicCourseOptions;
-	};
-
-	const awardOptions = createAwardOptions(awards);
-	const subjectOptions = createSubjectOptions(subjects);
-	const degreeOptions = createDegreeOptions(degrees);
-
-	const {
-		field: { value: awardsValue, onChange: awardsOnChange, ...restAwardsField },
-	} = useController({
-		name: "awards",
-		control,
-	});
-
 	useEffect(() => {
 		const handleKeyDown = (event) => {
 			if (event.key === "ArrowLeft") {
@@ -163,6 +30,159 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 			window.removeEventListener("keydown", handleKeyDown);
 		};
 	}, [returnStep, advanceStep]);
+
+	const methods = useForm({
+		defaultValues: {
+			degree: "",
+			personalProject: false,
+			subject: "",
+			academicCourse: "",
+			course: "",
+			//thumbnailFile: projectData.thumbnail ? [projectData.thumbnail] : [],
+			//projectFiles: projectData.uploadedContent ? projectData.uploadedContent.map(filePath => getFile(filePath)) : [],
+			// summaryFile: projectData.
+			externalLinks: [],
+		},
+	});
+
+	const {
+		register,
+		control,
+		watch,
+		handleSubmit,
+		formState: { errors },
+	} = methods;
+
+	// Obtenemos los datos de la API
+	const { degrees, getDegrees, awards, getAwards, subjects, getSubjects } = useAreas();
+
+	const [degreeOptions, setDegreeOptions] = useState([]);
+	const [academicCourseOptions, setAcademicCourseOptions] = useState([]);
+	const [subjectOptions, setSubjectOptions] = useState([]);
+	const [awardOptions, setAwardOptions] = useState([]);
+
+	// Obtenemos el grado actual para obtener las asignaturas de ese grado especifico
+	const currentDegree = watch("degree");
+
+	// Carga de opciones de DEGREE
+	useEffect(() => {
+		const getDegreeOptions = async () => {
+			try {
+				await getDegrees();
+				const options = degrees.map(({ id, name }) => ({
+					value: id,
+					label: name,
+				}));
+				setDegreeOptions(options);
+			} catch {
+				console.log("Error fetching options for degrees.");
+			}
+		};
+
+		getDegreeOptions();
+	}, []);
+
+	// Carga de opciones de ACADEMIC COURSE
+	useEffect(() => {
+		setAcademicCourseOptions(generateAcademicCourseOptions(2010));
+	}, []);
+
+	const generateAcademicCourseOptions = (startYear) => {
+		const currentMonth = new Date().getMonth();
+		const currentYear = new Date().getFullYear();
+		const academicCourseOptions = [];
+
+		const currentStartAcademicCourse = currentMonth < 8 ? currentYear - 1 : currentYear;
+
+		for (let year = startYear; year <= currentStartAcademicCourse; year++) {
+			const nextYear = year + 1;
+			const academicCourse = `${year}/${nextYear}`;
+			academicCourseOptions.unshift({ value: academicCourse, label: academicCourse });
+		}
+
+		return academicCourseOptions;
+	};
+
+	// Carga de opciones de AWARD
+	useEffect(() => {
+		const getAwardOptions = async () => {
+			try {
+				const awardsList = await getAwards();
+				const options = awardsList.map(({ id, name }) => ({
+					value: id,
+					label: name,
+				}));
+				setAwardOptions(options);
+			} catch {
+				console.log("Error fetching options");
+			}
+		};
+
+		getAwardOptions();
+	}, []);
+
+	// Carga de opciones de SUBJECT
+	useEffect(() => {
+		const getSubjectOptions = async () => {
+			if (currentDegree) {
+				try {
+					const subjectsList = await getSubjectsByDegree(currentDegree);
+					const options = subjectsList.map(({ id, name }) => ({
+						value: id,
+						label: name,
+					}));
+					setSubjectOptions(options);
+				} catch {
+					console.log("Error fetching options");
+				}
+			} else {
+				try {
+					const subjectsList = await getSubjects();
+					const options = subjectsList.map(({ id, name }) => ({
+						value: id,
+						label: name,
+					}));
+					setSubjectOptions(options);
+				} catch {
+					console.log("Error fetching options");
+				}
+			}
+		};
+
+		getSubjectOptions();
+	}, [currentDegree]);
+
+	const {
+		fields: linkFields,
+		append: appendLink,
+		remove: removeLink,
+		update: updateLink,
+	} = useFieldArray({
+		control,
+		name: "externalLinks",
+	});
+
+	const {
+		field: { value: academicCourseValue, onChange: academicCourseOnChange, ...restAcademicCourseField },
+	} = useController({
+		name: "academicCourse",
+		control,
+		rules: {
+			required: "Se requiere un curso académico",
+		},
+	});
+
+	// const awardOptions = createAwardOptions(awards);
+	// const subjectOptions = createSubjectOptions(subjects);
+	// const degreeOptions = createDegreeOptions(degrees);
+	// console.log(degrees);
+
+	const {
+		field: { value: awardsValue, onChange: awardsOnChange, ...restAwardsField },
+	} = useController({
+		name: "awards",
+		control,
+	});
 
 	const onSubmit = (data) => {
 		// Convert the degree and subject from an object to a string
@@ -232,18 +252,23 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 					{errors.subject && <p className="mb-2 mt-4 text-red-500 font-semibold">{errors.subject.message}</p>}
 				</div>
 
-				<div className="mb-4 md:col-span-2">
-					<Select
-						options={awardOptions}
-						isMulti
-						placeholder="Selecciona los premios"
-						value={awardsValue ? awardsValue.map((award) => awardOptions.find(({ value }) => value === award)) : awardsValue}
-						onChange={(options) => awardsOnChange(options ? options.map(({ value }) => value) : options)}
-						{...restAwardsField}
-						className="w-full border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-					/>
-					{errors.award && <p className="mb-2 mt-4 text-red-500 font-semibold">{errors.award.message}</p>}
-				</div>
+				<Controller
+					name="awards"
+					control={control}
+					defaultValue={[]}
+					render={({ field }) => (
+						<Select
+							{...field}
+							options={awardOptions}
+							isMulti
+							placeholder="Selecciona los premios"
+							className="w-full border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							// Ajuste para la selección múltiple
+							value={awardOptions.filter((option) => field.value.includes(option.value))}
+							onChange={(vals) => field.onChange(vals.map((val) => val.value))}
+						/>
+					)}
+				/>
 
 				<div className="mb-4 md:col-span-1">
 					<label className="block text-gray-700 text-sm font-bold mb-2">Curso académico</label>
