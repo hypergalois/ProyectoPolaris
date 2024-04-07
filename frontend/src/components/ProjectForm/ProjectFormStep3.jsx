1554b2069f98015d9b0317da3c6a4f1ba0a60aa1
hypergalois <jose.delgado2@live.u-tad.com>
2024-04-08 01:45:02 +0200

    Fix error in NotificationsProvider placement in app.js

--- frontend/src/components/ProjectForm/ProjectFormStep3.jsx
+++ frontend/src/components/ProjectForm/ProjectFormStep3.jsx
@@ -40,6 +40,7 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 		register,
 		control,
 		watch,
+		setValue,
 		handleSubmit,
 		formState: { errors },
 	} = methods;
@@ -51,6 +52,24 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 	const [academicCourseOptions, setAcademicCourseOptions] = useState([]);
 	const [subjectOptions, setSubjectOptions] = useState([]);
 	const [awardOptions, setAwardOptions] = useState([]);
+
+	// Si es proyecto personal deshabilitamos el campo de asignatura
+	const isPersonalProject = watch("personalProject");
+
+	// UseEffect para poner un valor a subject cuando sea proyecto personal
+	useEffect(() => {
+		if (isPersonalProject) {
+			setValue("subject", {
+				value: "Proyecto Personal",
+				label: "Proyecto Personal",
+			});
+		} else if (!isPersonalProject) {
+			setValue("subject", {
+				value: "Elige la asignatura",
+				label: "Elige la asignatura",
+			});
+		}
+	}, [isPersonalProject, setValue]);
 
 	// Obtenemos el grado actual para obtener las asignaturas de ese grado especifico
 	// Lo comento hasta que este el backend para las asignaturas por grado
@@ -178,6 +197,7 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 		control,
 	});
 
+	// Ojo que cuando este proyecto personal true, hay que ver que se hace con la asignatura
 	const onSubmit = (data) => {
 		const stepThreeData = {};
 
@@ -211,7 +231,7 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 			<Stepper currentStep={currentStep} />
 
 			<FormProvider {...methods}>
-				<form onSubmit={handleSubmit(onSubmit)} className="w-full bg-white rounded px-8 pt-6 mb-2 grid gap-4 md:grid-cols-2">
+				<form onSubmit={handleSubmit(onSubmit)} className="w-full bg-white rounded px-8 pt-6 mb-2 grid gap-4 md:grid-cols-2 mx-auto items-center">
 					{/* GRADO (CARRERA) */}
 					<div className="mb-2 w-full mx-auto col-span-2">
 						<div className="pt-2 flex flex-col">
@@ -234,15 +254,17 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 						</div>
 						{errors.degree && <p className="mb-2 mt-4 text-red-500 font-semibold">{errors.degree.message}</p>}
 					</div>
+
 					{/* PROYECTO PERSONAL */}
 					<div className="mb-2 w-full mx-auto">
 						<div className="flex pt-2 gap-4 items-center">
 							<label htmlFor="personalProject" className="text-blue-400 text-xs font-semibold">
-								Proyecto personal
+								Proyecto personal (no tiene asignatura)
 							</label>
 							<Controller name="personalProject" control={control} defaultValue={false} render={({ field }) => <input {...field} type="checkbox" className="form-checkbox" />} />
 						</div>
 					</div>
+
 					{/* ASIGNATURA */}
 					<div className="mb-2 w-full mx-auto">
 						<div className="flex flex-col">
@@ -250,7 +272,7 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 								name="subject"
 								control={control}
 								defaultValue=""
-								rules={{ required: "Se requiere una asignatura" }}
+								rules={{ required: !isPersonalProject ? "Se requiere una asignatura" : undefined }}
 								render={({ field }) => (
 									<Select
 										{...field}
@@ -259,12 +281,14 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 										className="outline-none border-none bg-transparent pt-2 text-blue-500 placeholder-blue-500 text-xs font-bold focus:outline-none focus:border-none focus:ring-0 focus:border-transparent"
 										menuPortalTarget={document.body}
 										styles={selectStylesCustom}
+										isDisabled={isPersonalProject}
 									/>
 								)}
 							/>
 						</div>
 						{errors.subject && <p className="mb-2 mt-4 text-red-500 font-semibold">{errors.subject.message}</p>}
 					</div>
+
 					{/* PREMIOS */}
 					<div className="mb-2 w-full mx-auto">
 						<div className="flex flex-col">
@@ -278,8 +302,9 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 										options={awardOptions}
 										isMulti
 										placeholder="Selecciona los premios"
-										className="w-full rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-										// Ajuste para la selección múltiple
+										className="outline-none border-none bg-transparent w-full pt-2 text-blue-500 placeholder-blue-500 text-xs font-bold focus:outline-none focus:border-none focus:ring-0 focus:border-transparent" // Estilos consistentes con los otros Selects
+										menuPortalTarget={document.body}
+										styles={selectStylesCustom}
 										value={awardOptions.filter((option) => field.value.includes(option.value))}
 										onChange={(vals) => field.onChange(vals.map((val) => val.value))}
 									/>
@@ -288,18 +313,30 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 						</div>
 						{errors.awards && <p className="mb-2 mt-4 text-red-500 font-semibold">{errors.awards.message}</p>}
 					</div>
+
 					{/* CURSO ACADÉMICO */}
-					<div className="mb-4 md:col-span-1">
-						<Select
-							options={academicCourseOptions}
-							value={academicCourseValue ? academicCourseOptions.find(({ value }) => value === academicCourseValue) : academicCourseValue}
-							onChange={(option) => academicCourseOnChange(option ? option.value : option)}
-							className="w-full  border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					<div className="mb-2 md:col-span-1">
+						<Controller
+							name="academicCourse"
+							control={control}
+							rules={{ required: "Se requiere un curso académico" }}
+							render={({ field }) => (
+								<Select
+									{...field}
+									options={academicCourseOptions}
+									value={academicCourseOptions.find((option) => option.value === field.value)}
+									onChange={(val) => field.onChange(val.value)}
+									className="outline-none border-none bg-transparent w-full pt-2 text-blue-500 placeholder-blue-500 text-xs font-bold focus:outline-none focus:border-none focus:ring-0 focus:border-transparent"
+									menuPortalTarget={document.body}
+									styles={selectStylesCustom}
+								/>
+							)}
 						/>
 						{errors.academicCourse && <p className="mb-2 mt-4 text-red-500 font-semibold">{errors.academicCourse.message}</p>}
 					</div>
+
 					{/* ENLACES EXTERNOS */}
-					<div className="mb-4 md:col-span-2 outline outline-blue-400">
+					<div className="mb-2 md:col-span-2 outline outline-blue-400">
 						<div className="m-3">
 							<label className="block text-blue-400 text-sm font-bold mb-2">Enlaces a recursos externos</label>
 							{linkFields.map((field, index) => (
@@ -333,39 +370,45 @@ const ProjectFormStep3 = ({ returnStep, advanceStep, currentStep, updateProjectD
 					</div>
 
 					{/* THUMBNAIL */}
-					<div className="mb-4 md:col-span-1">
-						<label className="block text-gray-700 text-sm font-bold mb-2">Thumbnail</label>
-						<DropzoneInput name="thumbnail" placeholder={"Arrastra y suelta tu thumbnail aquí."} maxFiles="1" accept={{ "image/jpeg": [], "image/png": [] }} />
-						{errors.thumbnail && (
-							<p className="mb-2 mt-4 text-red-500 font-semibold">
-								{errors.thumbnail.message}
-								{console.log(errors.thumbnail)}
-							</p>
-						)}
+					<div className="mb-2 md:col-span-1 outline outline-blue-400">
+						<div className="m-3">
+							<label className="block text-gray-700 text-sm font-bold mb-2">Thumbnail</label>
+							<DropzoneInput name="thumbnail" placeholder={"Arrastra y suelta tu thumbnail aquí."} maxFiles="1" accept={{ "image/jpeg": [], "image/png": [] }} />
+							{errors.thumbnail && (
+								<p className="mb-2 mt-4 text-red-500 font-semibold">
+									{errors.thumbnail.message}
+									{console.log(errors.thumbnail)}
+								</p>
+							)}
+						</div>
 					</div>
 
 					{/* RESUMEN */}
-					<div className="mb-4 md:col-span-1">
-						<label className="block text-gray-700 text-sm font-bold mb-2">Resumen del proyecto</label>
-						<DropzoneInput name="summary" placeholder={"Arrastra y suelta tu resumen (pdf/doc) aquí."} maxFiles="1" />
-						{errors.summary && (
-							<p className="mb-2 mt-4 text-red-500 font-semibold">
-								{errors.summary.message}
-								{console.log(errors.summary)}
-							</p>
-						)}
+					<div className="mb-2 md:col-span-1 outline outline-blue-400">
+						<div className="m-3">
+							<label className="block text-gray-700 text-sm font-bold mb-2">Resumen del proyecto</label>
+							<DropzoneInput name="summary" placeholder={"Arrastra y suelta tu resumen (pdf/doc) aquí."} maxFiles="1" />
+							{errors.summary && (
+								<p className="mb-2 mt-4 text-red-500 font-semibold">
+									{errors.summary.message}
+									{console.log(errors.summary)}
+								</p>
+							)}
+						</div>
 					</div>
 
 					{/* ARCHIVOS DEL PROYECTO */}
-					<div className="mb-4 md:col-span-2">
-						<label className="block text-gray-700 text-sm font-bold mb-2">Archivos del proyecto</label>
-						<DropzoneInput name="projectFiles" placeholder={"Arrastra y suelta tus archivos del proyecto aquí."} />
-						{errors.projectFiles && (
-							<p className="mb-2 mt-4 text-red-500 font-semibold">
-								{errors.projectFiles.message}
-								{console.log(errors.projectFiles)}
-							</p>
-						)}
+					<div className="mb-4 md:col-span-2 outline outline-blue-400">
+						<div className="m-3">
+							<label className="block text-gray-700 text-sm font-bold mb-2">Archivos del proyecto</label>
+							<DropzoneInput name="projectFiles" placeholder={"Arrastra y suelta tus archivos del proyecto aquí."} />
+							{errors.projectFiles && (
+								<p className="mb-2 mt-4 text-red-500 font-semibold">
+									{errors.projectFiles.message}
+									{console.log(errors.projectFiles)}
+								</p>
+							)}
+						</div>
 					</div>
 				</form>
 			</FormProvider>
