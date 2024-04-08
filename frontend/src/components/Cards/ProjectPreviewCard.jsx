8cea481dc19e48c9fd8442b49efbbdac25143d44
hypergalois <jose.delgado2@live.u-tad.com>
2024-04-08 02:21:45 +0200

    Add ProjectPreviewCard component

---
+++ frontend/src/components/Cards/ProjectPreviewCard.jsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+import Card from "@mui/joy/Card";
+import CardCover from "@mui/joy/CardCover";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import GroupIcon from "@mui/icons-material/Group";
+
+const ProjectCard = ({ project }) => {
+	return (
+		<>
+			<Card
+				sx={{ maxWidth: 545, minHeight: "300px", cursor: "pointer" }}
+				onClick={() => {
+					console.log("Click en el card");
+					console.log("project", project);
+					setOpenPopup(true);
+					// console.log(event.target);
+				}}
+			>
+				<CardCover>
+					<img src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800" srcSet="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800" loading="lazy" alt="" />
+				</CardCover>
+				<div
+					style={{
+						position: "absolute",
+						top: 10,
+						left: 10,
+						backgroundColor: "red",
+						color: "white",
+						padding: "4px 8px",
+						borderRadius: "4px",
+					}}
+				>
+					Animación
+				</div>
+				<CardCover
+					sx={{
+						background: "linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)",
+					}}
+				/>
+				<CardContent sx={{ justifyContent: "flex-end" }}>
+					<Typography level="h3" textColor="#fff">
+						{project.title}
+					</Typography>
+					<Typography startDecorator={<GroupIcon />} textColor="neutral.300">
+						Nombre de los alumnos
+					</Typography>
+				</CardContent>
+			</Card>
+		</>
+	);
+};
+
+export default ProjectCard;
